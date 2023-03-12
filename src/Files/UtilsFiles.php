fa441b38806ed5546cf3f849c05413dff3647d21
Davide Lanza <info@davidelanza.it>
2023-03-12 20:06:19 +0100

    Files/UtilsFiles: added store_json

--- src/Files/UtilsFiles.php
+++ src/Files/UtilsFiles.php
@@ -15,8 +15,7 @@ use Exception;
 /**
  * Load a JSON file
  *
- * @param string $directory    The JSON file directory.
- * @param string $filename     The JSON file name.
+ * @param string $path         The JSON file path.
  * @param array  $check_fields Optional. An array of fields which need to
  *                             be in the JSON file. Default to an empty array.
  *
@@ -25,17 +24,15 @@ use Exception;
  * @author Davide Lanza <davide.lanza@eikonproject.org>
  */
 function load_json(
-    string $directory,
-    string $filename,
+    string $path,
     array $check_fields = array()
 ) {
     // Read json file
-    $path = $directory . '/' . $filename;
-    $string = \file_get_contents($path);
+    $string = file_get_contents($path);
     if (!$string) {
         throw new Exception("No file '$path' found");
     }
-    $json = \json_decode($string, true);
+    $json = json_decode($string, true);
     // Check fields
     foreach ($check_fields as $field) {
         if (!array_key_exists($field, $json)) {
@@ -44,4 +41,31 @@ function load_json(
     }
     // Return loaded json
     return $json;
+}
+
+
+/**
+ * Store a JSON-like object to a JSON file
+ *
+ * @param mixed  $json         A JSON-like object.
+ * @param string $path         The file path for the JSON file created.
+ * @param array  $check_fields Optional. An array of fields which need to
+ *                             be in the JSON file. Default to an empty array
+ *
+ * @author Davide Lanza <davide.lanza@eikonproject.org>
+ */
+function store_json(
+    $json,
+    string $path,
+    array $check_fields = array()
+): void {
+    // Check fields
+    foreach ($check_fields as $field) {
+        if (!array_key_exists($field, $json)) {
+            throw new Exception("No field '$field' in '$path'");
+        }
+    }
+    // Write json file
+    $json = json_encode($json);
+    file_put_contents($path, $json);
 }
--- tests/Files/UtilsFilesTest.php
+++ tests/Files/UtilsFilesTest.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-require __DIR__ . "/../src/Files/UtilsFiles.php";
+require __DIR__ . "/../../src/Files/UtilsFiles.php";
 
 use PHPUnit\Framework\TestCase;
 use EikonPaginator\Files;
 
-final class FileUtilsTest extends TestCase
+final class UtilsFilesTest extends TestCase
 {
     public function testJsonCanBeLoaded(): void
     {
@@ -16,13 +16,11 @@ final class FileUtilsTest extends TestCase
         fwrite($temp, '{"field_1": "value_1"}');
         // Get file location
         $temp_path = stream_get_meta_data($temp)['uri'];
-        $temp_dir = dirname($temp_path);
-        $temp_fname = basename($temp_path);
         // Prepare expected output
         $expected = array("field_1" => "value_1");
         // Check actual output
         $check_fields = array("field_1");
-        $output = Files\load_json($temp_dir, $temp_fname, $check_fields);
+        $output = Files\load_json($temp_path, $check_fields);
         $this->assertSame($expected, $output);
         // Remove the temporary file
         fclose($temp);
@@ -35,12 +33,10 @@ final class FileUtilsTest extends TestCase
         fwrite($temp, '{"field_1": "value_1"}');
         // Get file location
         $temp_path = stream_get_meta_data($temp)['uri'];
-        $temp_dir = dirname($temp_path);
-        $temp_fname = basename($temp_path);
         // Check for wrong fields loaded
         $this->expectException(Exception::class);
         $check_fields = array("field_2");
-        Files\load_json($temp_dir, $temp_fname, $check_fields);
+        Files\load_json($temp_path, $check_fields);
         // Remove the temporary file
         fclose($temp);
     }
@@ -52,12 +48,10 @@ final class FileUtilsTest extends TestCase
         fwrite($temp, '{"field_1": "value_1"}');
         // Get file location
         $temp_path = stream_get_meta_data($temp)['uri'];
-        $temp_dir = dirname($temp_path);
-        $temp_fname = basename($temp_path);
         // Check for right and wrong fields loaded
         $this->expectException(Exception::class);
         $check_fields = array("field_1", "field_2");
-        Files\load_json($temp_dir, $temp_fname, $check_fields);
+        Files\load_json($temp_path, $check_fields);
         // Remove the temporary file
         fclose($temp);
     }
