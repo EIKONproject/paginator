e74f0bc699979a23541e1de31a823d4a8024563f
Davide Lanza <info@davidelanza.it>
2023-03-12 21:18:08 +0100

    Added _RequireAll mechanism

---
+++ src/_RequireAll.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once dirname(__FILE__) . '/Elements/_RequireAll.php';
+require_once dirname(__FILE__) . '/Files/_RequireAll.php';
+require_once dirname(__FILE__) . '/Spiders/_RequireAll.php';
