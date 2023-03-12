14d606bfb06501573a17d5fd5f2b674c5fd3a796
Davide Lanza <info@davidelanza.it>
2023-03-12 22:48:28 +0100

    UtilsFilesTest: added unit tests

--- src/Files/UtilsFiles.php
+++ src/Files/UtilsFiles.php
@@ -13,6 +13,29 @@ namespace EikonPaginator\Files;
 use Exception;
 
 /**
+ * Check JSON fields
+ *
+ * @param mixed  $json         A JSON-like object.
+ * @param array  $check_fields Optional. An array of fields which need to
+ *                             be in the JSON file. Default to an empty array
+ *
+ * @return bool TRUE if the check succeeds.
+ *
+ * @author Davide Lanza <davide.lanza@eikonproject.org>
+ */
+function check_fields_json($json, array $check_fields = array()): bool
+{
+    // Check fields
+    foreach ($check_fields as $field) {
+        if (!array_key_exists($field, $json)) {
+            throw new Exception("No field '$field' in '" . print_r($json, true) . "'");
+        }
+    }
+    return true;
+}
+
+
+/**
  * Load a JSON file
  *
  * @param string $path         The JSON file path.
@@ -31,11 +54,7 @@ function load_json(
     $string = file_get_contents($path);
     $json = json_decode($string, true);
     // Check fields
-    foreach ($check_fields as $field) {
-        if (!array_key_exists($field, $json)) {
-            throw new Exception("No field '$field' in '$path'");
-        }
-    }
+    check_fields_json($json, $check_fields);
     // Return loaded json
     return $json;
 }
@@ -57,11 +76,7 @@ function store_json(
     array $check_fields = array()
 ): void {
     // Check fields
-    foreach ($check_fields as $field) {
-        if (!array_key_exists($field, $json)) {
-            throw new Exception("No field '$field' in '$path'");
-        }
-    }
+    check_fields_json($json, $check_fields);
     // Write json file
     $json = json_encode($json);
     file_put_contents($path, $json);
--- tests/Files/UtilsFilesTest.php
+++ tests/Files/UtilsFilesTest.php
@@ -9,50 +9,68 @@ use EikonPaginator\Files;
 
 final class UtilsFilesTest extends TestCase
 {
-    public function testJsonCanBeLoaded(): void
+    public function test_JsonCheck_RightFields(): void
+    {
+        $json = array("field_1" => "value_1");
+        $check_fields = array("field_1");
+        $this->assertTrue(Files\check_fields_json($json, $check_fields));
+    }
+
+    public function test_JsonCheck_WrongFields(): void
+    {
+        // Check for wrong fields loaded
+        $json = array("field_1" => "value_1");
+        $check_fields = array("field_2");
+        $this->expectException(Exception::class);
+        Files\check_fields_json($json, $check_fields);
+    }
+
+    public function test_JsonCheck_RightAndWrongFields(): void
+    {
+        // Check for right and wrong fields loaded
+        $json = array("field_1" => "value_1");
+        $check_fields = array("field_1", "field_2");
+        $this->expectException(Exception::class);
+        Files\check_fields_json($json, $check_fields);
+    }
+
+
+    public function test_JsonLoad_Success(): void
     {
         // Prepare temporary input file
         $temp = tmpfile();
-        fwrite($temp, '{"field_1": "value_1"}');
-        // Get file location
+        fwrite($temp, '{"field_1":"value_1"}');
         $temp_path = stream_get_meta_data($temp)['uri'];
+
         // Prepare expected output
         $expected = array("field_1" => "value_1");
+
         // Check actual output
         $check_fields = array("field_1");
         $output = Files\load_json($temp_path, $check_fields);
         $this->assertSame($expected, $output);
+
         // Remove the temporary file
         fclose($temp);
     }
 
-    public function testJsonCheckWrongFieldsError(): void
+    public function test_JsonStore_Success(): void
     {
-        // Prepare temporary input file
+        // Prepare temporary expected file
         $temp = tmpfile();
-        fwrite($temp, '{"field_1": "value_1"}');
-        // Get file location
-        $temp_path = stream_get_meta_data($temp)['uri'];
-        // Check for wrong fields loaded
-        $this->expectException(Exception::class);
-        $check_fields = array("field_2");
-        Files\load_json($temp_path, $check_fields);
-        // Remove the temporary file
-        fclose($temp);
-    }
+        fwrite($temp, '{"field_1":"value_1"}');
+        $expected_file = stream_get_meta_data($temp)['uri'];
 
-    public function testJsonCheckRightWrongFieldsError(): void
-    {
-        // Prepare temporary input file
-        $temp = tmpfile();
-        fwrite($temp, '{"field_1": "value_1"}');
-        // Get file location
-        $temp_path = stream_get_meta_data($temp)['uri'];
-        // Check for right and wrong fields loaded
-        $this->expectException(Exception::class);
-        $check_fields = array("field_1", "field_2");
-        Files\load_json($temp_path, $check_fields);
-        // Remove the temporary file
+        // Write json to the output
+        $json = array("field_1" => "value_1");
+        $output_file = dirname($expected_file) . "/output.json";
+        Files\store_json($json, $output_file);
+
+        // Check that the files are the same
+        $this->assertSame(file_get_contents($expected_file), file_get_contents($output_file));
+
+        // Remove the temporary files
+        unlink($output_file);
         fclose($temp);
     }
 }
