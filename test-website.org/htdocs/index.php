bc97ab6915e4cde3b50ed893ae51038c6aedf75e
Davide Lanza <info@davidelanza.it>
2023-03-16 23:41:53 +0100

    test-website.org: index with sorters and checkboxes

--- test-website.org/htdocs/index.php
+++ test-website.org/htdocs/index.php
@@ -4,20 +4,62 @@
 require_once dirname(dirname(__FILE__)) . '/Packages/Paginator/_RequireAll.php';
 
 use EikonPaginator\Elements\Set;
-use EikonPaginator\Spiders\SetSpider;
+use EikonPaginator\Spiders;
 
-$cache_root = dirname(__FILE__) . "/Cache/spiders/";
-
+// Define the spiders needed in the page
+$spiders_cache = dirname(__FILE__) . "/Cache/spiders/";
 $pages_set_1 = new Set(dirname(__FILE__) . '/pages_set_1/');
-$spider_category_1 = new SetSpider($pages_set_1, "EikonPaginator\Elements\Page", $cache_root);
-
+$spider_set_1 = new Spiders\SetSpider($pages_set_1, "EikonPaginator\Elements\Page", $spiders_cache);
 ?>
 
 <head>
 </head>
 
 <body>
-    <pre><?php print_r($spider_category_1->get()); ?></pre>
+    <h1>Checkboxes</h1>
+    <?php // Prepare spider
+    $checks_spider = $spider_set_1->get();
+$checks_spider = Spiders\sort_categories($checks_spider, "shortcode");
+$checks_spider = Spiders\sort_entities($checks_spider, "shortcode");
+?>
+    <h4>Categories <code>"name"</code> checkboxes</h4>
+    <?php echo Spiders\Html\category_checkboxes("first", $spider_set_1->get(), "name"); ?>
+    <h4>Entities <code>"name"</code> checkboxes</h4>
+    <?php echo Spiders\Html\entity_checkboxes("first", $spider_set_1->get(), "name"); ?>
+    <h4>Entities <code>"date"</code> checkboxes</h4>
+    <?php echo Spiders\Html\entity_checkboxes("first", $spider_set_1->get(), "date"); ?>
+
+    <h1>Full random array</h1>
+    <?php // Prepare spider for print
+$full_random = $spider_set_1->get();
+$full_random = Spiders\random_entities($full_random);
+$full_random = Spiders\random_categories($full_random);
+$full_random = Spiders\filter_categories(
+    $full_random,
+    "shortcode",
+    array("pages_category_1", "pages_category_3")
+); ?>
+    <pre><?php print_r($full_random); ?></pre>
+
+    <h1>Filtered by categories <code>"shortcode" in ("pages_category_1", "pages_category_3")</code></h1>
+    <?php // Prepare spider for print
+$filtered_cats = $spider_set_1->get();
+$filtered_cats = Spiders\filter_categories(
+    $filtered_cats,
+    "shortcode",
+    array("pages_category_1", "pages_category_3")
+); ?>
+    <pre><?php print_r($filtered_cats); ?></pre>
+
+    <h1>Filtered by entities <code>"name" in ("Page 1", "Page 6", "Page 8")</code></h1>
+    <?php // Prepare spider for print
+$filtered_cats = $spider_set_1->get();
+$filtered_cats = Spiders\filter_entities(
+    $filtered_cats,
+    "name",
+    array("Page 1", "Page 6", "Page 8")
+); ?>
+    <pre><?php print_r($filtered_cats); ?></pre>
 </body>
 
 </html>
